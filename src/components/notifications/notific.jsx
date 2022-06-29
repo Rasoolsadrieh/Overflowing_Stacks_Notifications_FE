71a1aa21d75654a8cf419c5578da62df4b04dd45
Veronica Leslie <v.leslie423@gmail.com>
2022-06-29 09:56:36 -0400

    tried implementing some new things, needs lots of work

---
+++ src/components/notifications/notific.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+
+export default function MoneyRecieved(){
+
+    const[sendMoney, setSendMoney] = useState()
+    onst[user, setUser] = useState()
+    const IntervalExample = () => {
+        const [seconds, setSeconds] = useState(0);
+        useEffect(() => {
+            const interval = setInterval(() => {
+              setSeconds(seconds => seconds + 1);
+            }, 10);
+            return () => clearInterval(interval);
+          }, []);
+
+    
+    return(
+
+        <>
+        
+        
+        
+        
+        
+        </>
+    )
+}
