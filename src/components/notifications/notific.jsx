dfb76956a36a6ff18b936792f34d636fcd133716
Veronica Leslie <v.leslie423@gmail.com>
2022-06-30 00:23:00 -0400

    made the function check every 10 seconds if isReceived is true and got the message to print when reciever is sent money

--- src/components/notifications/notific.jsx
+++ src/components/notifications/notific.jsx
@@ -1,28 +1,60 @@
 import { useState } from "react"
+import Payment from "../sendmoney/transfermoney";
 
 
 export default function MoneyRecieved(){
 
-    const[sendMoney, setSendMoney] = useState()
-    onst[user, setUser] = useState()
-    const IntervalExample = () => {
-        const [seconds, setSeconds] = useState(0);
+    const[moneyRecieved, setMoneyRecieved] = useState()
+    const[user, setUser] = useState()
+    
+    if (Payment (isRecieved == false)) {
+     
+      } else {
         useEffect(() => {
-            const interval = setInterval(() => {
-              setSeconds(seconds => seconds + 1);
-            }, 10);
-            return () => clearInterval(interval);
-          }, []);
+            const fetchData = async () => isRecieved == true && <p> You have recieved money! </p>;)}
+            componentDidMount() {
+                this.timer = setInterval(()=> this.getMoney(), 10000);
+                return () => clearInterval(interval);
+              };
+            }
+            try {
+                const response = await axios.put(`${url}/notification`, user);
+                const response2 = await axios.put(`${url}/notification`, MoneyRecieved);
+                console.log(response.data);
+                console.log(response2.data);
+                setMoneyRecieved(!isRecieved)
+    
+    
+             
+            } catch (error) {
+                console.error(error.response.data);
+                console.error(error.response2.data);
+                
+            }
+        }
+     
+            };
+              
+        //       componentWillUnmount() {
+        //         this.timer = null; 
+        //       }
+              
+        //       getMoney() {
+        //         fetch(this.getIsRecieved('https://overflowingstacksweb.azurewebsites.net/notification'))
+        //           .then(isRecieved => result.json())
+        //           .then(isReceived => this.setState({ money: result }));
+        //       };
+        //     }
+        // onPayment={this.findAll}
+        // update
+      
+      
+      return (
+        <div>
+          <p> Money has been added to your account </p>
+        </div>
+      );
+    
 
     
-    return(
-
-        <>
-        
-        
-        
-        
-        
-        </>
-    )
-}
+ 
