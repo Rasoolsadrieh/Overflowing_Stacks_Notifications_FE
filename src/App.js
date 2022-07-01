334447c32624d7e0995cfaa4db034ecc6d69d186
Veronica Leslie <v.leslie423@gmail.com>
2022-07-01 15:27:16 -0400

    made changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import { createContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MoneyRecieved from "./components/notifications/notific";
+import MoneyRecieved from "./components/notifications/notificationbar";
 import Notifications from "./components/notifications/notificationbar";
-;
+import Welcome from "./components/welcome/welcomepage"
 
 export const userContext = createContext();
 
 function App() {
 
-  const [user, setUser] = useState({username: "Guest" })
+  
+  const [user, setUser] = useState({username: "Guest", isReceived: false })
+ 
   return (
     <>
-    <Notifications/>
+  
     <BrowserRouter>
          <userContext.Provider value={[user, setUser]}>
              <Routes>
-                {/* <Route path="" element={<Welcome />} />
-                <Route path="/" element={<Welcome />} /> */
-              <Route path="/notifications" element={<Notifications />} />}
+                 <Route path="" element={<Welcome />} />
+                <Route path="/" element={<Welcome />} />
+               <Route path="/notifications" element={<Notifications />} />
               <Route path="/notification" element={<MoneyRecieved />} />
-              
+               
               </Routes>
          </userContext.Provider>
      </BrowserRouter>
--- src/components/welcome/welcomepage.jsx
+++ src/components/welcome/welcomepage.jsx
@@ -1,11 +1,14 @@
+
 import WelcomeNavBar from "./welcomepagenavbar";
 
 
 
 export default function Welcome(){
+  
     return(
         
         <>
+        
         <WelcomeNavBar/>
         </>
     )
